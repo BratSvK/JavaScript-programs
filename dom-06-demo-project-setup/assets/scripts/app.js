5da54024e544e063c8480e510180d82c667ba705
Martin Gajdos <martingajdos.zs@gmail.com>
2020-09-23 17:46:23 +0200

    add newMovie and remove a movie

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -4,6 +4,48 @@ const backDrop = document.getElementById("backdrop");
 const cancelBtn = modalWindow.querySelector("button");
 const addConfirmBtn = cancelBtn.nextElementSibling;
 const userInputs = modalWindow.querySelectorAll("input");
+const entryTextSection = document.getElementById("entry-text");
+const listRoot = document.getElementById("movie-list");
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  } else {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const deleteMovieHandler = (movieId) => {
+  let movieIndex = 0;
+  for (const movie of movies) {
+    if (movie.id === movieId) {
+      break;
+    }
+    movieIndex++;
+  }
+  movies.splice(movieIndex,1);
+  listRoot.children[movieIndex].remove();
+
+};
+
+const renderNewMovieElement = (id, title, imageUrl, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  // set HTML content
+  newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+    <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie-element__info">
+    <h2>${title}</h2>
+    <p>${rating}/5 stars</p>
+    </div>
+  `;
+  newMovieElement.addEventListener("click", deleteMovieHandler.bind(null, id));
+  listRoot.append(newMovieElement);
+};
 
 const toggleBackgroundDrop = () => {
   backDrop.classList.toggle("visible");
@@ -25,9 +67,9 @@ const backdropClickHandler = () => {
 };
 
 const resetInput = () => {
-    for (const input of userInputs) {
-        input.value = "";
-    }
+  for (const input of userInputs) {
+    input.value = "";
+  }
 };
 // to be consistent handler to eventListeners
 const addMovieHandler = () => {
@@ -35,8 +77,22 @@ const addMovieHandler = () => {
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
   if (titleValue.trim() === "" || imageUrlValue.trim() === "" || ratingValue.trim() === "" || +ratingValue < 1 || +ratingValue > 5) {
-      alert("Please enter valid inputs(rating 1 to 5)")
+    alert("Please enter valid inputs(rating 1 to 5)");
   }
+
+  const newMovie = {
+    id: Math.random().toString(), //only for this demo
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log("movies: ", movies);
+  toggleModalAddMovie();
+  resetInput();
+  renderNewMovieElement(newMovie.id, newMovie.title, newMovie.image, newMovie.rating);
+  updateUI();
 };
 
 addBtn.addEventListener("click", toggleModalAddMovie);
